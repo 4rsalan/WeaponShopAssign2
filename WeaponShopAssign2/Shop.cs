20f9e2d195e531359798e6ea68c788d0b784eafb
Andrew Cobb <andrew@cobbs-web.com>
2019-03-22 15:32:52 -0400

    delete and buy method added in shop

--- WeaponShopAssign2/Shop.cs
+++ WeaponShopAssign2/Shop.cs
@@ -74,5 +74,67 @@ namespace WeaponShopAssign2
             InOrderTraversal(curr.right);
         }
 
+        private BSTNode getNode (string wName)
+        {
+            return getNodeHelper(root, wName);
+        }
+        private BSTNode getNodeHelper(BSTNode curr, string wName)
+        {
+            if (curr == null) return null;
+            if (curr.weapon.Peek().weaponName == wName) return curr;
+            if (curr.weapon.Peek().weaponName.CompareTo(curr.weapon.Peek().weaponName) < 0) return getNodeHelper(curr.left, wName);
+            return getNodeHelper(curr.right, wName);
+        }
+
+        public Stack<Weapon> buyWeapon(string wName, int amount)
+        {
+            BSTNode node = getNode(wName);
+            Stack<Weapon> purchestWeapons = new Stack<Weapon>();
+            if (node == null) return null;
+            for (int i = 0; i < amount || node.weapon.Count == 0; i++)
+                purchestWeapons.Push(node.weapon.Pop());
+            if (node.weapon.Count == 0)
+                deleteNode(wName);
+            return purchestWeapons;
+        }
+
+        public void deleteNode(string wName)
+        {
+            root = deleteNodeHelper(root, wName);
+        }
+        private BSTNode deleteNodeHelper(BSTNode curr, string wName)
+        {
+            if (curr == null) return null;
+
+            if (curr.weapon.Peek().weaponName == wName)
+            {
+                //Case 1 delete a leaf node
+                if (curr.left == null && curr.right == null)
+                    return null;
+                //Case 2 delete a node that has a single child
+                if (curr.left == null)
+                    return curr.right;
+                if (curr.left == null)
+                    return curr.right;
+                //Case 3 two children (replace with in order sucessor) 
+                //find next in order node
+                BSTNode nextInOrder = curr.right;
+                BSTNode prev = null;
+                while (nextInOrder.left != null)
+                {
+                    prev = nextInOrder;
+                    nextInOrder = nextInOrder.left;
+                }
+                if (prev != null)prev.left = nextInOrder.right;
+                nextInOrder.right = curr.right;
+                nextInOrder.left = curr.left;
+                return nextInOrder;
+            }
+            if (curr.weapon.Peek().weaponName.CompareTo( wName) < 0)
+                curr.left = deleteNodeHelper(curr.left, wName);
+            else if (curr.weapon.Peek().weaponName.CompareTo(wName) > 0)
+                curr.right = deleteNodeHelper(curr.right, wName);
+            return curr;
+        }
     }
 }
